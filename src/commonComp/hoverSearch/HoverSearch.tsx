06a5c1dad26505241aa6df0883b88d73a28735f8
nischal <nischal@hubbler.mobi>
2021-01-23 19:47:00 +0530

    added scroll feature

---
+++ src/commonComp/hoverSearch/HoverSearch.tsx
@@ -0,0 +1,117 @@
+import React, { FC, useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import './hoverSearch.scss'
+import hoverSearchData from './hoverSearchData'
+
+
+
+export const getHighlightedText = (
+    text: string,
+    highlight: string
+): JSX.Element => {
+
+    // highlights the searched match text
+
+    const parts = text.split(new RegExp(`(${highlight})`, 'gi'));
+    return (
+        <>
+            {parts.map((part, i) => (
+                <span
+                    key={`hightlight_${part + i}`}
+                    className={part.toLowerCase() === highlight ? "highlight_text" : ""}
+                >
+                    {part}
+                </span>
+            ))}
+        </>
+    );
+};
+
+const HoverSearch: FC = (): JSX.Element => {
+
+    const [searchValue, setSearchValue] = useState<string>("")
+    const suggestionContainerRef = useRef<HTMLDivElement>(null);
+    const [currentHoverIndex, setCurrentHoverIndex] = useState<number>(-1)
+    const [currentHoverId, setCurrentHoverId] = useState<string>("")
+
+
+    const filterData = (inputData: string) => {
+        if (inputData.length === 0) return []
+        const filteredCompany = hoverSearchData.filter(data => {
+            const allValues = JSON.parse(JSON.stringify(Object.values(data)))
+            allValues.shift()
+            return allValues.join("; ").toLowerCase().includes(inputData)
+        })
+        return filteredCompany
+    }
+
+    const onSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchValue(e.target.value.toLowerCase().trim())
+    }
+
+    const onKeyPressHandler = (e: React.KeyboardEvent<HTMLDivElement>) => {
+
+        const allfilteredIds = filterData(searchValue).map(data => data._id)
+        // const currentHoveredIndex = allfilteredIds.indexOf(currentHoverId)
+
+        if (e.key === "ArrowUp" && currentHoverIndex !== 0) {
+            setCurrentHoverIndex(currentHoverIndex - 1)
+            setCurrentHoverId(allfilteredIds[currentHoverIndex - 1])
+        } else if (e.key === "ArrowDown" && allfilteredIds.length !== currentHoverIndex + 1) {
+            setCurrentHoverIndex(currentHoverIndex + 1)
+            setCurrentHoverId(allfilteredIds[currentHoverIndex + 1])
+
+        }
+    }
+
+    useEffect(() => {
+        const childNodes = suggestionContainerRef.current?.childNodes
+        const getElement = document.getElementById(currentHoverId)
+        if (childNodes?.length) {
+            getElement?.scrollIntoView();
+        }
+    }, [currentHoverIndex])
+
+
+    const onMouseEnterHandler = useCallback((index: number, id: string) => {
+        if (currentHoverIndex !== index) {
+            setCurrentHoverIndex(index)
+            setCurrentHoverId(id)
+        }
+    }, [currentHoverIndex])
+
+
+
+
+
+
+    return (
+        <div className="hoverSearch">
+            <div className="hoverSearch_main" onKeyDown={onKeyPressHandler}>
+
+                <input className="hoverSearch_input" onChange={onSearch} />
+                <div className="hoverSearch_suggestion_main">
+                    <div className="hoverSearch_suggestion_container" ref={suggestionContainerRef}>
+                        {filterData(searchValue).map((singleData, index) => (
+                            <div className={`single_suggestion ${currentHoverIndex === index ? "single_suggestion_hover" : ""}`}
+                                key={singleData._id}
+                                id={singleData._id}
+                                onMouseEnter={() => onMouseEnterHandler(index, singleData._id)}
+                            >
+                                <h4 className="company">{getHighlightedText(singleData.company, searchValue)}</h4>
+                                <p className="name">{getHighlightedText(singleData.name, searchValue)}</p>
+                                <p className="address">{getHighlightedText(singleData.address, searchValue)}</p>
+                            </div>
+                        )
+                        )}
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+
+    );
+}
+
+export default HoverSearch;
---
+++ src/commonComp/hoverSearch/hoverSearchData.ts
@@ -0,0 +1,77 @@
+interface IhoverSearch {
+    name: string
+    _id: string
+    address: string
+    company: string
+}
+
+const hoverSearchData: IhoverSearch[] = [
+    {
+        "_id": "600b2262c3c90b04f6f58a25",
+        "name": "Vasquez Mueller",
+        "company": "EYERIS",
+        "address": "298 Lott Avenue, Kiskimere, Puerto Rico, 680"
+    },
+    {
+        "_id": "600b2262d1dbbcd8afe40cde",
+        "name": "Yang Guzman",
+        "company": "IMAGEFLOW",
+        "address": "358 Louis Place, Campo, Indiana, 7435"
+    },
+    {
+        "_id": "600b22620e4bfd91866cc461",
+        "name": "Awadhesh Rollins",
+        "company": "ECOLIGHT",
+        "address": "154 Aberdeen Street, Iola, Virginia, 7167"
+    },
+    {
+        "_id": "600b22628ef3a37a304dc7bd",
+        "name": "Shubham Shukla",
+        "company": "ACCUFARM",
+        "address": "894 Hemlock Street, National, Guam, 6140"
+    },
+    {
+        "_id": "600b2262c9fbac396f2b3a28",
+        "name": "Ravi Singh",
+        "company": "MENBRAIN",
+        "address": "971 Montgomery Place, Maplewood, Louisiana, 6903"
+    },
+    {
+        "_id": "600b22621edcc1f532d34aea",
+        "name": "Rahul Kumar",
+        "company": "ROOFORIA",
+        "address": "485 Grafton Street, Noblestown, Tennessee, 392"
+    },
+    {
+        "_id": "600b226255f40b102531b2e9",
+        "name": "Nischal Singh",
+        "company": "ISOLOGICA",
+        "address": "268 Cambridge Place, Kohatk, Kansas, 2615"
+    },
+    {
+        "_id": "600b2262d98692dd778e4691",
+        "name": "Mcintosh Richardson",
+        "company": "CALCU",
+        "address": "271 Gatling Place, Faywood, District Of Columbia, 4761"
+    },
+    {
+        "_id": "600b226248d36e6fa12d70f3",
+        "name": "Johnnie Hartman",
+        "company": "FLUMBO",
+        "address": "927 Midwood Street, Woodlands, Iowa, 5862"
+    },
+    {
+        "_id": "600b226285295ea43c02efeb",
+        "name": "Pittman Fowler",
+        "company": "ELEMANTRA",
+        "address": "600 Balfour Place, Veguita, Nebraska, 6003"
+    },
+    {
+        "_id": "600b2262c79c714c8c2b000a",
+        "name": "Ochoa Johns",
+        "company": "WEBIOTIC",
+        "address": "451 Bedford Avenue, Goochland, Marshall Islands, 4691"
+    }
+]
+
+export default hoverSearchData
